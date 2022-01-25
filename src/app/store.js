f1596d7ac905a67a8f1b14ef3b3b07e1f286dd50
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-25 18:47:52 +0900

    taskSlice作成

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import loginReducer from "../features/login/loginSlice";
+import taskReducer from "../features/task/taskSlice";
 
 export const store = configureStore({
   reducer: {
     login: loginReducer,
+    task: taskReducer,
   },
 });
